2f9c897e8527e55c24cb283d973c916ef4bb65c0
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-05-16 17:54:55 +0200

    fetched initail pokemon list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ListDisplay from './components/ListDisplay';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return <div class='text-red-500'>PokeDex</div>;
+  // - PokemonList.js
+  // - the amount of pokemon in existence
+  let PokemonCount = 898;
+
+  const [pokemonList, setPokemonList] = useState({});
+
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${PokemonCount}`
+      );
+
+      const data = await response.json();
+
+      setPokemonList(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <ListDisplay pokemonList={pokemonList} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
