3c5686fea0a89c8b0ffbafaac8ad7f609c87b2f9
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-05-16 18:04:05 +0200

    added basic error handling for fetched pokemon data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import ListDisplay from './components/ListDisplay';
+import axios from 'axios';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  const [error, setError] = useState(false);
   // - PokemonList.js
   // - the amount of pokemon in existence
   let PokemonCount = 898;
@@ -13,15 +15,16 @@ function App() {
 
   const fetchPokemon = async () => {
     try {
-      const response = await fetch(
+      const data = await axios(
         `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=${PokemonCount}`
       );
 
-      const data = await response.json();
-
-      setPokemonList(data);
+      setPokemonList(data.data);
+      setError(false);
+      console.log(data.data);
     } catch (err) {
       console.log(err);
+      setError(true);
     }
   };
 
@@ -32,7 +35,12 @@ function App() {
   return (
     <>
       <Header />
-      <ListDisplay pokemonList={pokemonList} />
+      {!error && <ListDisplay pokemonList={pokemonList} />}
+      {error && (
+        <h2 className='text-blue-500 w-2/3 mx-auto text-center p-2 shadow border-gray-200 rounded-md'>
+          Failed to fetch pokemon data, please try again.
+        </h2>
+      )}
       <Footer />
     </>
   );
