4eeb1d34ad1aa2d2a2b618439ff59c4368bb7117
AyoCodess <ayo.daniel.adesanya@gmail.com>
2022-05-17 22:22:47 +0200

    added pokemon stats to vs page

--- src/components/VsContainer.js
+++ src/components/VsContainer.js
@@ -7,6 +7,7 @@ export default function VsContainer({ data }) {
         <div className='space-y-8 sm:space-y-12'>
           <ul className='mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6'>
             {data.map((pokemon, i) => {
+              console.log(pokemon.data);
               //   console.log(pokemon.data.sprites.front_default);
               return (
                 <li key={i}>
@@ -16,10 +17,83 @@ export default function VsContainer({ data }) {
                       src={pokemon.data.sprites.front_default}
                       alt=''
                     />
+                    <button
+                      //   onClick={() =>
+                      //     setSavedPokemon((prev) => {
+                      //       if (prev.includes(pokemon)) {
+                      //         console.log('pokemon already saved');
+                      //         return prev;
+                      //       } else {
+                      //         return prev.concat(pokemon);
+                      //       }
+                      //     })
+                      //   }
+                      className='flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-xs font-medium bg-red-100 rounded-full'>
+                      remove
+                    </button>
                     <div className='space-y-2'>
-                      <div className='text-xs font-medium lg:text-sm'>
-                        <h3>{pokemon.data.name}</h3>
-                        {/* <p className='text-indigo-600'>{person.role}</p> */}
+                      <div className=' font-medium'>
+                        <h2 className='text-xl'>{pokemon.data.name}</h2>
+                        <p>
+                          ID:{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.id}
+                          </span>
+                        </p>
+                        <p>
+                          Height:{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.height}
+                          </span>
+                        </p>
+                        <p>
+                          Weight:{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.weight}
+                          </span>
+                        </p>
+                        <p>
+                          Type:{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.types.map((type) => {
+                              return <span>{`${type.type.name} `}</span>;
+                            })}
+                          </span>
+                        </p>
+                        <p>
+                          Base Experience::{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.base_experience}
+                          </span>
+                        </p>
+                        <p>
+                          Abilities:{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.abilities.map((ability) => {
+                              return <span>{`${ability.ability.name} `}</span>;
+                            })}
+                          </span>
+                        </p>
+
+                        <hr className='mt-2' />
+                        <p className='mt-2'>
+                          <span className='font-bold text-red-500'>
+                            Base stats:
+                          </span>{' '}
+                          <span className='text-indigo-600'>
+                            {pokemon.data.stats.map((stat) => {
+                              console.log(stat.stat.name);
+                              return (
+                                <p className='block'>
+                                  <span className='text-black'>
+                                    {`${stat.stat.name}:`}{' '}
+                                  </span>
+                                  {`${stat.base_stat} `}
+                                </p>
+                              );
+                            })}
+                          </span>
+                        </p>
                       </div>
                     </div>
                   </div>
